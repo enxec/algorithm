42eb8564ab77a6ffacf90e80d319c2cfb5700348
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-16 23:48:12 +0900

    [level 0] Title: 369게임, Time: 1.11 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int order) {
-        int answer = 0;
-        String strOrder = "" + order;
-        for (int i = 0; i < strOrder.length(); i++) {
-            char tempChar = strOrder.charAt(i);
-
-            if (tempChar == '3' || tempChar == '6' || tempChar == '9') {
-                answer++;
-            }
-        }
-        return answer;
+        return (int) Arrays.stream(String.valueOf(order).split("")).map(Integer::parseInt).filter(i -> i == 3 || i == 6 || i == 9).count();
     }
 }
