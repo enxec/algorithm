e8a34e2fc6b9af71016e60941cc82cfed803307a
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-16 23:20:01 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 2.91 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] array) {
+        List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+        int max = list.stream().max(Integer::compareTo).orElse(0);
+        int index = list.indexOf(max);
+        return new int[] {max, index};
+    }
+}
