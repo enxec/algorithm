0a99dc6ece40b06c4bb5beba426a530dc2b98769
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-16 23:29:24 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        while(true) {
+            if((n * i) % 6 == 0) return (n * i) / 6;
+            i++;
+        }
+    }
+}
