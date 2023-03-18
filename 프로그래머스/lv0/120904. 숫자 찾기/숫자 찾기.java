3e9de7fe2d923fff311638006e4ffee730495c7f
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-18 18:40:52 +0900

    Update 숫자 찾기.java

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        int n = String.valueOf(num).indexOf(String.valueOf(k));
-        return n==-1?-1:n+1;
+        return ("!" + num).indexOf(String.valueOf(k));
     }
 }
