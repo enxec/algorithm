a1d73dd5f1b753bb2ee9deb44baef00662d72212
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-18 03:26:35 -0700

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2;i<=n;i++){
+            int temp=0;
+            for(int j=i;j>=1;j--){
+                if (i%j==0) { temp++; 
+                    if(temp>2) {
+                        answer++;
+                        break;
+                    }}
+            }
+            temp=0;
+        }
+        return answer;
+    }
+}
