2368885198462d06287fb47335bd07348e773fda
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-19 19:14:19 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 5.48 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,18 +1,16 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        int[] arr = new int[26];
-        for (int i = 0; i < 26; i++) {
-            arr[i] = 0;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 97] += 1;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (arr[i] == 1) {
-                answer += (char)(i + 97);
-            }
-        }
-        return answer;
+        return Arrays.stream(s.split(""))
+                .collect(Collectors.groupingBy(s1 -> s1))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() <= 1)
+                .map(Map.Entry::getKey)
+                .sorted()
+                .collect(Collectors.joining());
     }
 }
