16488ce7bae7a946899899deefd8a288ae9e6a1c
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-19 20:20:48 +0900

    Update 7의 개수.java

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] array) {
-        return (int) Arrays.stream(Arrays.toString(array).replaceAll("[^0-9]", "")
-                .split(""))
+        return (int) Arrays.stream(Arrays.toString(array).split(""))
                 .filter(i -> i.equals("7"))
                 .count();
     }
