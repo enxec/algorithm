935100d3b3f48ff80df82ca4cdd6bb99646c3398
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-19 17:02:03 +0900

    [level 0] Title: k의 개수, Time: 525.18 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,16 +1,11 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int count = 0;
-        String strK = String.valueOf(k);
-        for (int l = i; l <= j; l++) {
-            String value = String.valueOf(l);
-            if (value.contains(strK)) {
-                String[] array = value.split("");
-                for (String alpha : array) {
-                    if (alpha.equals(strK)) count++;
-                }
-            }
+        String str = "";
+        
+        for(int a = i; a <= j; a++) {
+            str += a + "";
         }
-        return count;
+
+        return str.length() - str.replace(k + "", "").length();
     }
 }
