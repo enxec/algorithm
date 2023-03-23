47365dc4cba19c892ebd56a870815d27ef145e8e
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-23 20:33:07 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -1,20 +1,23 @@
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer=0;
-        for(int i=0; i<dic.length; i++) {               
-            int check=0;                                
-            for(int j=0; j<spell.length; j++) {         
-                if(dic[i].indexOf(spell[j])==-1) {      
-                    check=-1;                           
-                    answer=2;                           
-                    break;                              
+        int answer = 2;
+        
+        for(String dicS : dic) {
+
+            boolean isRight = true;
+            for(String spellS : spell) {
+                if(dicS.indexOf(spellS) == -1) {
+                    isRight = false;
+                    break;
                 }
             }
-            if(check==0) {
-                answer=1;
+
+            if(isRight) {
+                answer = 1;
                 break;
-            }     
+            }
         }
+
         return answer;
     }
 }
