9a25fc5894761bb02dc75b73e22af8c2ee0ed42a
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-03-25 12:11:30 +0900

    [level 0] Title: 등수 매기기, Time: 0.25 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,19 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[][] score) {
-        float[] average = new float[score.length];
-        for(int i = 0; i < score.length; i++) {
-            average[i] = (float)((score[i][0] + score[i][1]) / 2.0);
+        List<Integer> scoreList = new ArrayList<>();
+        for(int[] t : score){
+            scoreList.add(t[0] + t[1]);
         }
-        
+        scoreList.sort(Comparator.reverseOrder());
+
         int[] answer = new int[score.length];
-        for(int j = 0; j < score.length; j++) {
-            int order = 1;
-            for(int k = 0; k < score.length; k++) {
-                if(average[j] < average[k]) order++;
-                answer[j] = order;
-            }
+        for(int i=0; i<score.length; i++){
+            answer[i] = scoreList.indexOf(score[i][0] + score[i][1])+1;
         }
-        
         return answer;
     }
 }
