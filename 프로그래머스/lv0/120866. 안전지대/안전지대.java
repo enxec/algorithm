1a172ebc9ee0abff8ac2a7b7197d6042c009d279
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-06-17 09:24:37 +0900

    [level 0] Title: 안전지대, Time: 0.50 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,31 +1,35 @@
-class Solution {
-    public int solution(int[][] board) {
-        int[][] boomBoard = new int[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1) checkExplosion(i, j, boomBoard);
+public class Solution {
+
+    public int solution(int[][] param) {
+        int safeZone = 0;
+        // 위험지역 Set
+        for (int i = 0; i < param.length; i++) {
+            for (int j = 0; j < param.length; j++) {
+                if (param[i][j] == 1)
+                    setArea(param, i, j);
+            }
+        }
+        // 안전지역 Count
+        for (int i = 0; i < param.length; i++) {
+            for (int j = 0; j < param.length; j++) {
+                if (param[i][j] == 0)
+                    safeZone++;
             }
         }
 
-        int count = 0;
-        for (int[] array : boomBoard) {
-            for (int value : array) {
-                if (value == 0) count++;
-            }
-        }
-        return count;
+        return safeZone;
     }
 
-    static void checkExplosion(int x, int y, int[][] boomBoard) {
-        int explosionX;
-        int explosionY;
-        int[] aroundX = {0, -1, -1, -1, 0, 0, 1, 1, 1};
-        int[] aroundY = {0, -1, 0, 1, -1, 1, -1, 0, 1};
-        for (int i = 0; i < 9; i++) {
-            explosionX = x + aroundX[i];
-            explosionY = y + aroundY[i];
-            if (explosionX < boomBoard.length && explosionY < boomBoard.length)
-                if (explosionX >= 0 && explosionY >= 0) boomBoard[explosionX][explosionY] = 1;
+    void setArea(int[][] param, int x, int y) {
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                try {
+                    if (param[x + i][y + j] == 0)
+                        param[x + i][y + j] = 2;
+                } catch (Exception e) {
+                }
+            }
         }
     }
+
 }
