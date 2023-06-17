e61c80437f6b1fed04b2063ff2ea0c81ae045470
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-06-17 11:08:43 +0900

    [unrated] Title: 카운트 다운, Time: 2.20 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,11 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = new int[start - end + 1];
-        
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = start--;
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray();
     }
 }
