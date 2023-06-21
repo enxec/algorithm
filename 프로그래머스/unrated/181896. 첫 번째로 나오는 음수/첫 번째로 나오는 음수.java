ab3015935162dc3cf8df3eed08e8037f859c36e1
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-06-21 22:28:02 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 2.58 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -2,13 +2,6 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] num_list) {
-        int answer = -1;
-        for(int i = 0; i<num_list.length;i++){
-            if(num_list[i]<0){
-                answer = i;
-                break;
-            }
-        }
-        return answer;
+        return IntStream.range(0, num_list.length).filter(i -> num_list[i] < 0).findFirst().orElse(-1);
     }
 }
