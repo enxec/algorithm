b83690891b15cdbddece3aba1a7413945f36a898
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-06-21 21:56:20 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length + 1];
-        int last = 0;
-        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-            last = num_list[num_list.length - 1] - num_list[num_list.length - 2];
-        } else {
-            last = num_list[num_list.length - 1] * 2;
-        }
-        
-        for (int i = 0; i < num_list.length; i++) {
+        int[] answer = new int[num_list.length+1];
+
+        for(int i = 0; i < num_list.length; i++) {
             answer[i] = num_list[i];
         }
-        answer[answer.length - 1] = last;
+
+        int last = num_list[num_list.length-1];
+        int before = num_list[num_list.length-2];
+
+        answer[answer.length-1] = last > before ? last - before : last*2;
+
         return answer;
     }
 }
