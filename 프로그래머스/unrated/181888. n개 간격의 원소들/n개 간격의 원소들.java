f1bd64e8081c213880fd01e1e138d3dcb3a33ab5
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2023-07-06 18:59:41 +0900

    [unrated] Title: n개 간격의 원소들, Time: 3.56 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,18 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer;
-        int index = 0;
-        
-        if(num_list.length % n == 0) {
-            answer = new int[num_list.length / n];
-        } else {
-            answer = new int[num_list.length / n + 1];
-        }
-        
-        
-        for (int i = 0; i < num_list.length; i += n) {
-            answer[index++] = num_list[i];
-        }
-        return answer;
+        return IntStream.iterate(0, i -> i < num_list.length, i -> i + n).map(i -> num_list[i]).toArray();
     }
 }
