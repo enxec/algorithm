2eb197d6837882ac99e7ef3340277336ec16f10c
이원모 <dev21mo@gmail.com>
2024-09-24 16:12:21 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 1.19 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,15 +1,9 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] num_list) {
-        int[] answer = new int[5];
-        
+    public int[] solution(int[] num_list) {    
         Arrays.sort(num_list);
         
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = num_list[i];
-        }
-        
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
